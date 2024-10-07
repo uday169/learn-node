b1c28a6672b67a7935949a04049932564493cf42
Uday Pratap <udaypratap169@gmail.com>
2024-10-07 10:26:05 +0530

    implemented export

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+const { calculateSum, calculateMultiply } = require("./calculate");
+
 var name = "Namaste Uday";
 
 var a = 10;
 var b = 20;
 
 console.log(name);
-console.log(a + b);
+
+calculateSum(a, b);
+
+calculateMultiply(a, b);
---
+++ calculate/sum.js
@@ -0,0 +1,6 @@
+function calculateSum(a, b) {
+  const sum = a + b;
+  console.log(sum);
+}
+
+module.exports = { calculateSum };
